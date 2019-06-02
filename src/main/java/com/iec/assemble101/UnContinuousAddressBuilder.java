e23f6c486fcc8d09e0fb5fe5f5d85b43628b55ad
张雨 <1600351631@qq.com>
2019-06-02 17:49:28 +0800

    完成101规约的组装

---
+++ src/main/java/com/iec/assemble101/UnContinuousAddressBuilder.java
@@ -0,0 +1,152 @@
+package com.iec.assemble101;
+
+import com.iec.utils.Util;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.function.Function;
+
+/**
+ * 信息体元素地址不连续的报文建造者。
+ *
+ * @param <T> 信息体的类型，
+ */
+public class UnContinuousAddressBuilder<T> extends VariableLengthPacket {
+
+
+    /**
+     * 信息体元素地址。
+     */
+    private ArrayList<Integer> informationAdress;
+    /**
+     * 信息体元素。
+     * 包含多个元素
+     */
+    private ArrayList<T> informosomes;
+    /**
+     * 单个信息体的字节长度。默认为2
+     */
+    private int infoLength;
+
+    public UnContinuousAddressBuilder() {
+        this(9, 0, 1, 0, 9, 2);//地址连续的报文
+    }
+
+    /**
+     * 信息体元素地址不连续的报文
+     *
+     * @param con            控制域
+     * @param address        地址
+     * @param TI             类型标识TI(推荐使用TypeIdentifier类的code)
+     * @param asduAddress    应用数据单元公共地址
+     * @param transferReason 传送原因(推荐使用TransferReason类的code)
+     * @param infoLength     信息体元素值的字节长度
+     */
+    public UnContinuousAddressBuilder(int con, int address, int TI, int asduAddress, int transferReason, int infoLength) {
+        this(con, address, TI, asduAddress, transferReason, infoLength, null, -1);
+    }
+
+    private UnContinuousAddressBuilder(int con, int address, int TI, int asduAddress, int transferReason, int infoLength, Date dateTime, int qualifier) {
+        super(con, address, TI, 0, asduAddress, transferReason);
+        this.dateTime = dateTime;
+        this.infoLength = infoLength;
+        this.qualifier = qualifier;
+        this.informosomes = new ArrayList<>();
+        this.informationAdress = new ArrayList<Integer>();
+    }
+
+    public UnContinuousAddressBuilder builderCon(int con) {
+        this.con = con;
+        return this;
+    }
+
+    public UnContinuousAddressBuilder builderAddress(int address) {
+        this.address = address;
+        return this;
+    }
+
+    public UnContinuousAddressBuilder builderTi(int ti) {
+        this.TI = ti;
+        return this;
+    }
+
+    public UnContinuousAddressBuilder builderAsduAdress(int asduAdsress) {
+        this.asduAddress = asduAdsress;
+        return this;
+    }
+
+    public UnContinuousAddressBuilder builderTransferReason(int transferReason) {
+        this.transferReason = transferReason;
+        return this;
+    }
+
+    public UnContinuousAddressBuilder addInfoAddress(int informationAdress) {
+        this.informationAdress.add(informationAdress);
+        return this;
+    }
+
+    public UnContinuousAddressBuilder addInfo(int informationAdress, T informosome) {
+        this.informationAdress.add(informationAdress);
+        this.informosomes.add(informosome);
+        return this;
+    }
+
+    public UnContinuousAddressBuilder builderQualifier(int qualifier) {
+        this.qualifier = qualifier;
+        return this;
+    }
+
+    public UnContinuousAddressBuilder builderDateTime(Date date) {
+        this.dateTime = date;
+        return this;
+    }
+
+    @Override
+    public String builder() {
+        return this.builder(x -> Integer.parseInt(x.toString()));
+    }
+
+    public String builder(Function<T, Integer> fun) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("68");
+        int length = 8;//控制域截至到应用服务单元地址的长度
+        length += 2;//加上信息体地址的长度
+        length += (informosomes.size() * infoLength);//加上信息体个数乘以信息体的单位长度
+        if (qualifier != -1) length++; //有限定词（品质描述词）
+        if (dateTime != null) length += 7;
+        builder.append(String.format("%02X", length));
+        builder.append(String.format("%02X", length));
+        builder.append("68");
+        builder.append(String.format("%02X", this.con));
+        builder.append(Util.getAddressStr(this.address));
+        builder.append(String.format("%02X", this.TI));
+        builder.append(String.format("%02X", (this.SQ << 7) + this.informationAdress.size()));
+        builder.append(String.format("%02X", this.transferReason));
+        builder.append(Util.getAddressStr(asduAddress));
+        for (int i = 0; i < informationAdress.size(); i++) {
+            builder.append(Util.getAddressStr(this.informationAdress.get(i)));
+            if (this.informosomes.size() > i)
+                builder.append(Util.getInfoStr(fun.apply(this.informosomes.get(i)), infoLength));
+        }
+        if (qualifier != -1) builder.append(String.format("%02X", qualifier));
+        if (dateTime != null) builder.append(Util.date2HStr(this.dateTime));
+        long sum = 0;
+        sum += this.con % 256;
+        sum += Util.add2long(this.address) % 256;
+        sum += this.TI % 256;
+        sum += (((this.SQ << 7) + this.informationAdress.size()) % 256);
+        sum += this.transferReason % 256;
+        sum += Util.add2long(this.asduAddress) % 256;
+        // TODO: 2019-05-31 双字节地址的校验和
+        for (int i = 0; i < informationAdress.size(); i++) {
+            sum += Util.add2long(this.informationAdress.get(i)) % 256;
+            if (this.informosomes.size() > i)
+                sum += fun.apply(this.informosomes.get(i)) % 256;
+        }
+        if (this.dateTime != null) sum += Util.CP56Time2Long(this.dateTime) % 256;
+        if (this.qualifier != -1) sum += this.qualifier % 256;
+        builder.append(String.format("%02X", sum % 256));
+        builder.append("16");
+        return builder.toString();
+    }
+}
--- src/test/java/com/iec/test/Analysis101Test.java
+++ src/test/java/com/iec/test/Analysis101Test.java
@@ -2,18 +2,28 @@ package com.iec.test;
 
 import com.iec.analysis.exception.*;
 import com.iec.analysis.protocol101.Analysis;
+import com.iec.assemble101.ContinuousAddressBuilder;
+import com.iec.assemble101.UnContinuousAddressBuilder;
+import com.iec.assemble101.VariableLengthPacket;
+import com.iec.utils.Util;
 import org.junit.Test;
+import sun.util.resources.cldr.CalendarData;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Analysis101Test {
 
 
     @Test
-    public void fixed()  {
+    public void FixedLengthFrameAnalysisTest() {
 
         String analysis = null;
 
         try {
-            analysis = Analysis.analysis("104901004a16");
+            analysis = Analysis.analysis("10 49 01 00 4a 16".replaceAll(" ", ""));
         } catch (LengthException e) {
             e.printStackTrace();
         } catch (UnknownTransferReasonException e) {
@@ -29,11 +39,13 @@ public class Analysis101Test {
         }
         System.out.println(analysis);
     }
+
+
     @Test
-    public void elongate(){
+    public void VariableLengthFrameParsingTest() {
         String analysis = null;
         try {
-            analysis = Analysis.analysis("680b0b68d3010064010a01000000145816");
+            analysis = Analysis.analysis("68  0f  0f  68  53  01  00  88  01  06  01  00  03  50  58  D4  44  40  80  67  16".replaceAll(" ", ""));
         } catch (LengthException e) {
             e.printStackTrace();
         } catch (CustomException e) {
@@ -49,5 +61,74 @@ public class Analysis101Test {
         }
         System.out.println(analysis);
     }
+
+    @Test
+    public void buliderTest1() {
+        /**
+         * 描述：这里测试变长帧中信息体地址不连续的时候的报文，并且第一个信息体元素值为空。
+         * 报文示例：召唤命令（具体报文结构请参照附件1：章节5.1.1 Page23）
+         *          报文选自附件1：章节8.4 第三部分 Page83
+         *
+         * 注明：这里采用这条报文作为测试实例是因为这条报文是一条变长的、SQ(信息体元素地
+         *      址是否连续标志)为0、并且信息体元素没有值。
+         *      也就是说报文有一个信息体，且只有信息体元素地址，没有值，且在校验位之前有召
+         *      唤限定词(0x14)。
+         *
+         * 适用范围：总召唤过程，时钟同步过程，复位进程，初始化进程。
+         */
+        /**
+         * 报文简单解析：
+         * 68  0B  0B  68  73  01  00  64  01  06  01  00  00  00  14  F4  16
+         * 73  控制域  115
+         * 01 00 地址域  1
+         * 64 TI 100
+         * 01 sq=0 信息体个数1
+         * 06 传送原因 6
+         * 01 00 asdu地址
+         * 00 00 第一个信息体的地址
+         * 14 召唤限定词 20
+         * F4 校验位
+         */
+        UnContinuousAddressBuilder<Integer> unContinuousAddressBuilder = new UnContinuousAddressBuilder<Integer>(115, 1, 100, 1, 6, 0);
+        unContinuousAddressBuilder.addInfoAddress(0);//添加第一个信息体的地址
+        unContinuousAddressBuilder.builderQualifier(20);//添加召唤限定词
+        String s = unContinuousAddressBuilder.builder();
+        System.out.println(s);
+        System.out.println("68  0B  0B  68  73  01  00  64  01  06  01  00  00  00  14  F4  16".replaceAll(" ", "").equals(s));
+    }
+
+    @Test
+    public void buliderTest2() throws ParseException {
+        /**
+         * 描述：这里测试边长帧中信息体地址不连续的时候的报文，并且附带一个时标的实例。
+         * 报文示例：带CP56Time2a时标的单点信息（具体报文结构请参照附件1：章节5.2 b) Page28）
+         *
+         * 注明：这里采用这条报文作为测试实例是因为这条报文是一条变长的、SQ(信息体元素地
+         *      址是否连续标志)为0、有一个完整的信息体，并且附带一个时标。
+         *
+         */
+        /**
+         * 68  12  12  68  d3  01  00  1e  01  03  01  00  21  00  01  9a  e5  19  0a  83  03  10  51  16
+         * 报文长度24字节
+         * d3 控制域 211
+         * 01 00 地址域 1
+         * 1e TI 30
+         * 01 可变结构限定词 sq=0；信息体个数为1
+         * 03 传输原因COT 3
+         * 01 00  ASDU地址 1
+         * 21 00  第一个信息体的地址 21
+         * 1 第一个信息体的值 1
+         * 9a e5 19 0a 83 03 10  CP56Time2a时标 2016-03-03,10:25:58.778
+         *
+         */
+        UnContinuousAddressBuilder<Integer> unContinuousAddressBuilder = new UnContinuousAddressBuilder<>(211, 1, 30, 1, 3, 1);
+        unContinuousAddressBuilder.addInfo(33, 1);
+        unContinuousAddressBuilder.builderDateTime(new SimpleDateFormat("yyyy-MM-dd,HH:mm:ss.SSS").parse("2016-03-03,10:25:58.778"));
+        String builder = unContinuousAddressBuilder.builder();
+        System.out.println(builder.toString());
+        System.out.println("68  12  12  68  D3  01  00  1E  01  03  01  00  21  00  01  9A  E5  19  0A  83  03  10  51  16".replaceAll(" ", ""));
+    }
+
+
 
 }
